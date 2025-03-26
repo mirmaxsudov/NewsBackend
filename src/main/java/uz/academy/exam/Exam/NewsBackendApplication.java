395ac7b28b2716a77dc99bb0e40a81871ea0af99
Abdurahmon Mirmaxsudov <abdurahmonmirmaxsudov2804@gmail.com>
2025-03-26 22:57:01 +0500

    init project

---
+++ src/main/java/uz/academy/exam/Exam/NewsBackendApplication.java
@@ -0,0 +1,13 @@
+package uz.academy.exam.Exam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uz/academy/exam/Exam/NewsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.academy.exam.Exam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
